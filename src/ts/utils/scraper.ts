2f57bcf01c6fd898797867d9fd4fac650c943f33
DoormatIka <trashcanartistlily@gmail.com>
2023-01-21 17:16:20 +0800

    progress with the twitch API

--- src/ts/utils/scraper.ts
+++ src/ts/utils/scraper.ts
@@ -1,27 +1,64 @@
-// twitch title       | class   = CoreText-sc-1txzju1-0 gBAboc
-// twitch description | class   = ScCoreLink-sc-16kq0mq-0 jSrrlW tw-link
-// twitch tags        | class[] = InjectLayout-sc-1i43xsx-0 eUxEWt
-// twitch pfp (src)   | class   = InjectLayout-sc-1i43xsx-0 bEwPpb tw-image tw-image-avatar (very last index)
-// twitch game        | class   = CoreText-sc-1txzju1-0 hfMGmo
-import { JSDOM } from "jsdom";
-import axios from "axios";
+// twitch title       | class   = h2.gBAboc
+// twitch tags        | class[] = div.eUxEWt
+// twitch pfp (src)   | class[] = img.tw-image-avatar (very last index)
+// twitch genre       | class   = span.hfMGmo
+// twitch offline     | class   = p.jiQuvm (not working)
+// twitch name        | class   = h1.CoreText-sc-1txzju1-0
 
+// twitch container   | class   = div.bhLqhW
 
-axios("http://books.toscrape.com/").then(a => {
-    const js = new JSDOM(a.data);
-    const res = js.window.document.getElementsByClassName("nav nav-list")
+/* no API option for the class
 
-    const list = res.item(0)?.children.item(0);
-    const gl_children = list?.children
+IM GONNA USE A FUCKING SCRAPER INSTEAD OF THE API I SWEAR TO GOD
 
-    if (!gl_children) return;
-    const genre_list = gl_children.item(1)?.children
-    if (!genre_list) return;
+const blacklist = [
+    'googlesyndication.com',
+    'adservice.google.com',
+]
 
+async function main() {
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+    page.setRequestInterception(true);
+
+    page.on("request", request => {
+        const url = request.url();
+        const req = request.resourceType();
+
+        if (
+            blacklist.some(domain => url.includes(domain)) ||
+            req === "font" || req === "image" || req === "media"
+        ) {
+            request.abort();
+        } else {
+            request.continue();
+        }
+    });
     
+    const moist = await getData("https://www.twitch.tv/moistcr1tikal", page);
+    console.log(moist);
+    const osu   = await getData("https://www.twitch.tv/osulive", page);
+    console.log(osu);
 
-    for (const child of genre_list) {
-        console.log( child.outerHTML )
-        console.log( child.querySelector("a")?.href )
+    await browser.close();
+}
+*/
+/*
+async function getData(twitch: string, page: Page) {
+    await page.goto(twitch, { waitUntil: "load" });
+    await page.waitForSelector("img.tw-image-avatar");
+
+    const twitch_title = await page.$("h2.gBAboc");
+    const image_avatar = await page.$$("img.tw-image-avatar");
+
+    return {
+        title: await twitch_title?.evaluate(s => s.innerText),
+        avatar: await image_avatar.at(-1)?.evaluate(s => s.src),
+        isOnline: twitch_title ? true : false
     }
-})
+}
+*/
+
+// main().then(() => {})
