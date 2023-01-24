5807e2203462d1a6d31e3decc737be7349172db0
DoormatIka <trashcanartistlily@gmail.com>
2023-01-25 01:59:03 +0800

    finished the basis of shutdown functionality on twitter.ts

--- src/ts/lib/buffer.ts
+++ src/ts/lib/buffer.ts
@@ -1,6 +1,5 @@
 // this file is to store "Memory" to hard drive and get it from hard drive on start up
 import { JsonDB, Config } from "node-json-db"
-import tweets from "./mockTweets.json";
 // handles offline functionality
 // and missed tweets or posts
 export class ExternalStorage<T> {
@@ -15,32 +14,4 @@ export class ExternalStorage<T> {
     add(element: T): void {
         this.buffer.push(element);
     }
-}
-// testing
-/* emulating how i'll get the tweets with the massive data
-async function main() {
-    const twit = new Storage<{ id: number, val: number }>()
-    const max_tweets = 5;
-    let time = 11;
-    while (true) {
-        const fetched = fetch(max_tweets, time);
-        if (fetched.length === 0) {
-            break;
-        }
-        fetched.forEach(c => {
-            twit.add(c)
-        })
-        time += max_tweets
-    }
-    console.log(twit)
-}
-main()
-*/
-// Mocking how Twitter gets tweets
-// time - it maps to the id basically
-// max_tweets - how much you wanna get (sorted by newest (higher id))
-function fetch(max_tweets: number, time: number) {
-    const selected = tweets.findIndex(v => v.id === time)
-    if (!selected) return [];
-    return tweets.slice(time, selected + max_tweets);
 }
--- src/ts/lib/twitter.ts
+++ src/ts/lib/twitter.ts
@@ -1,9 +1,7 @@
-import { TwitterApi, TwitterApiReadOnly, TweetV2PaginableTimelineResult } from "twitter-api-v2";
-import { ExternalStorage } from "./buffer"
+import { TwitterApi, TwitterApiReadOnly, TweetV2, TweetV2PaginableTimelineResult } from "twitter-api-v2";
 import api from "../../../secrets/api.json"
-
-import EventEmitter from "events"
-import TypedEmitter from "typed-emitter"
+import EventEmitter from "events";
+import TypedEmitter from "typed-emitter";
 
 type TwitEvents = {
     tweeted: (text: string, sensitive?: boolean) => void,
@@ -34,7 +32,7 @@ export class TwitterUser {
     private event: TypedEmitter<TwitEvents>
     private client: TwitterApiReadOnly
     private innateMemory: TwitterMemory
-    private innateStorage: Array<TweetV2PaginableTimelineResult> = []
+    public innateStorage: Array<TweetV2> = []
     constructor(
         bearerToken: string,
         public userId: string,
@@ -64,6 +62,7 @@ export class TwitterUser {
                 return;
             }
             // if memory tweetId is not equal to current tweetId
+            // aka. you tweeted something!
             if (this.innateMemory.tweet_id !== currentTweet.id) {
                 this.event.emit("tweeted", currentTweet.text, currentTweet.possibly_sensitive); 
                 this.innateMemory.tweet_id = currentTweet.id;
@@ -77,12 +76,11 @@ export class TwitterUser {
         // where you'll get the eventListener to get tweets after it turns on
     }
 
-    // helper functions
-    public async getUserTweets(
-        options?: {
+    // helper functions (turn to private later)
+    public async getUserTweets(options?: {
             includeReplies: boolean, 
             includeRetweets: boolean 
-        }) {
+        }, nextToken?: string) {
         
         const excluded = this.convertIncludeToExclude(options);
         const user = await this.client.v2.userByUsername(this.userId)
@@ -90,10 +88,28 @@ export class TwitterUser {
 
         // this function returns the user's post history
         const tweets = await this.client.v2.userTimeline(id, {
-            max_results: 5, // do i get 100 tweets to search and get missed tweets?
+            max_results: 5, // use pagination
             exclude: excluded,
+            pagination_token: nextToken
         })
         return tweets.data;
+    }
+
+    public async getDelayedTweets(tweetID: string, options?: {
+        includeReplies: boolean, 
+        includeRetweets: boolean
+    }) {
+        let token: string | undefined;
+        let tw: TweetV2PaginableTimelineResult;
+        while (true) {
+            tw = await this.getUserTweets(options, token);
+            if (!tw.meta.next_token) break;
+
+            const found = tw.data.find(v => v.id === tweetID);
+            this.innateStorage.push(...tw.data)
+            if (found) break;
+            token = tw.meta.next_token;
+        }
     }
 
     private convertIncludeToExclude(option?: { includeReplies: boolean, includeRetweets: boolean }) {
@@ -110,7 +126,9 @@ export class TwitterUser {
 
 
 async function main() { // testing
-    const lilyn = new TwitterUser(api.twitter_bearerToken, "bannedvtmemes", 5000);
-    const tweets = await lilyn.getUserTweets()
+    const lilyn = new TwitterUser(api.twitter_bearerToken, "danimato_", 5000);
+    // getting all tweets after tweet ID: 1608485058733826050
+    await lilyn.getDelayedTweets("1608485058733826050");
+    console.log( lilyn.innateStorage )
 }
 main()
