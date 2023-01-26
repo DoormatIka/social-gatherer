e432989ed0bf26ad47c152014973ed320c270255
DoormatIka <trashcanartistlily@gmail.com>
2023-01-26 20:00:01 +0800

    rewrite needed

--- src/ts/lib/adapters/youtube.ts
+++ src/ts/lib/adapters/youtube.ts
@@ -1,37 +1,51 @@
 import { YouTubeChannel, YoutubeJSON } from "../yt"
 
-// J is JSON type
-// O is Object type
-class Group<J, O> {
+// lib/adapter.ts
+/**
+ * Converts JS Objects to Class Objects to be used with a JSON-based database
+ * @generic P - Parameter type
+ * @generic M - Memory type
+ * @generic O - Object type
+ */
+class Adapter<P, M, O extends User<P, M>> {
     // using Generics and initializing a class with it
-    constructor(private object: new () => O) {}
-    JSONToObject() {
-        new this.object();
+    constructor(private object: new (parameters: P) => O) {}
+    JSONToObject(...parameters: P[]) {
+        const objects: O[] = []
+        for (const parameter of parameters) {
+            objects.push(new this.object(parameter));
+        }
+        return objects; 
+    }
+}
+// lib/user.ts
+class User<P, M> {
+    protected innateMemory?: M;
+    constructor(public parameters: P) {}
+    /**
+     * @type M - swss 
+     */
+    set memory(memory: M) {
+        this.innateMemory = memory
     }
 }
 
-function objectMaker<O, T, K>(
-    object: new (
-        innateMemory: T, 
-        parameters: K) => O,
-    memory: T, parameters: K
-) {
-    return new object(memory, parameters)
+// lib/youtube.ts
+type YoutubeParameters = {
+    username: string
 }
-class User<T, K> {
-    constructor(
-        protected innateMemory: T,
-        public parameters: K
-    ) {} 
-    setInnateMemory(memory: T) {}
+type YoutubeMemory = {
+    lastVideoRecorded: string
 }
 
-class Youtube<T, K> extends User<T, K> {
-    constructor(innateMemory: T, parameters: K) {
-        super(innateMemory, parameters);
+class Youtube extends User<YoutubeParameters, YoutubeMemory> {
+    constructor(parameters: YoutubeParameters) {
+        super(parameters);
     }
-    setInnateMemory() {}
 }
 
-const user = new Youtube<string, string>("memory", "parameter");
-console.log( objectMaker<Youtube<string, string>, string, string>(Youtube<string, string>, "memory", "parameter") )
+// main.ts
+const lilyn = new Youtube({ username: "lilyn" });
+lilyn.memory = { lastVideoRecorded: Math.random().toString(10) }
+console.log( lilyn )
+
