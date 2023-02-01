f3e8f0c333a3870a87e15256ef50bb8fcd9e6eb7
DoormatIka <trashcanartistlily@gmail.com>
2023-02-01 21:21:32 +0800

    removed test

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -23,21 +23,3 @@ export {
 
     isYoutube, isTwitter, isTwitch, isManager
 }
-
-async function main() {
-    const db = new Cache("cache");
-    const youtube = await db.get("youtube");
-
-    if (isYoutube(youtube)) { // type checking, remove this for vanilla node.js
-        youtube.forEach(async v => {
-            // enable everything in the cache
-            await v.enableVideoEvent();
-            v.getEventEmitter()
-                .on("newUpload", (id, author, title, duration) => {
-                    console.log(`Video ID: ${id}, ${author}, ${title}`);
-                })
-        })
-        db.pushYoutube(youtube)
-    }
-}
-main()
